b0726ca081259fcab23673ecfc0d68f1ec386318
OLOLADE CHARLES ELEGBEDE <generalpepemac@General-Pepes-Mac-mini.local>
2026-03-12 14:05:20 -0600

    Use Render PORT environment variable

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def index():
 
     return render_template("index.html", weather=weather, forecast=forecast, error=error)
 
+import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render assigns this automatically
     app.run(host="0.0.0.0", port=port, debug=True)
